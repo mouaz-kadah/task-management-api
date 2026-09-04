2d88a4d686bce3df6e9f156742168dedcb4dc61f
mouaz <muaz.kadah@gmail.com>
2026-09-05 01:42:02 +0300

    Use environment variables for database URL

--- database.py
+++ database.py
@@ -3,7 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # غيّر username و password بـ الـ credentials تبعك
-DATABASE_URL = "postgresql://admin:PASSWORD@dpg-dadk3cn40uj673bq8uv0-a:5432/task_management_joq5"
+import os
+DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
