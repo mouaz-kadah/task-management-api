8c9562906f518066d78d0ed50d4bd3cd5f513cc5
mouaz <muaz.kadah@gmail.com>
2026-09-05 22:54:21 +0300

    Revert to MySQL for PythonAnywhere deployment

--- database.py
+++ database.py
@@ -3,8 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # غيّر username و password بـ الـ credentials تبعك
-import os
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = "mysql+mysqlconnector://root:121538@localhost:3306/task_management"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
