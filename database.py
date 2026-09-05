3a038fe617461c0b953d1fbec735cc33b5e19420
mouaz <muaz.kadah@gmail.com>
2026-09-05 23:29:00 +0300

    Configure database for PostgreSQL deployment

--- database.py
+++ database.py
@@ -3,7 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # غيّر username و password بـ الـ credentials تبعك
-DATABASE_URL = "mysql+mysqlconnector://root:121538@localhost:3306/task_management"
+import os
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
